e3d1a63b9accf9703a2182d7d2af4d8822bdaf1c
dstp <dstp71@gmail.com>
2023-06-27 12:10:36 -0400

    update for nala

--- sway.sh
+++ sway.sh
@@ -1,68 +1,68 @@
 #!/bin/sh
 
-sudo apt install git micro zram-tools neofetch exa timeshift
+sudo apt install git micro zram-tools neofetch exa
 
 # Sway installation for Debian Bookworm/Testing
-sudo apt install sway waybar swaylock swayidle swaybg
+sudo nala install sway waybar swaylock swayidle swaybg
 
 # implements the KDE idle protocol
-sudo apt install swayidle
+sudo nala install swayidle
 
 # Appearance manager
-sudo apt install lxappearance
+sudo nala install lxappearance
 
 # Networking etc
-sudo apt install policykit-1-gnome network-manager network-manager-gnome
+sudo nala install policykit-1-gnome network-manager network-manager-gnome
 
 # Thunar
-sudo apt install thunar thunar-archive-plugin thunar-volman file-roller
+sudo nala install thunar thunar-archive-plugin thunar-volman file-roller
 
 # terminal
-sudo apt install xfce4-terminal
+sudo nala install xfce4-terminal
 
 # protocols
-sudo apt install mtools dosfstools acpi acpid avahi-daemon gvfs-backends
+sudo nala install mtools dosfstools acpi acpid avahi-daemon gvfs-backends
 sudo systemctl enable acpid
 sudo systemctl enable avahi-daemon
 
 # grim (screenshots in Wayland) and slurp (select a region in wayland) - kinda like scrot
-sudo apt install grim slurp
+sudo nala install grim slurp
 
 # mako - like dunst
-sudo apt install mako-notifier
+sudo nala install mako-notifier
 
 # wofi - confusingly similar to rofi
-sudo apt install wofi
+sudo nala install wofi
 
 # wayland protocols library
-sudo apt install wayland-protocols
+sudo nala install wayland-protocols
 
 # backwards compatible 
-sudo apt install xwayland
+sudo nala install xwayland
 
 # nvidia you may need libnvidia-egl-wayland-dev
 
 # GTK layer shell
-sudo apt install libgtk-layer-shell-dev
+sudo nala install libgtk-layer-shell-dev
 
 # Support for the screenshot, screencast, and possibly remote-desktop xdg-desktop-portal interfaces for wlroots based compositors. 
-sudo apt install xdg-desktop-portal-wlr
+sudo nala install xdg-desktop-portal-wlr
 
 # GTK layer shell - wl-clipboard implements two Wayland clipboard utilities, wl-copy and wl-paste
 # dex executes .desktop files - jq = lightweight and flexible command-line JSON processor
-sudo apt install brightnessctl wl-clipboard dex jq
+sudo nala install brightnessctl wl-clipboard dex jq
 
 # text editor
-sudo apt install l3afpad
+sudo nala install l3afpad
 
 # Create folders in user directory (eg. Documents,Downloads,etc.)
 xdg-user-dirs-update
 
 # build essential
-sudo apt install build-essential
+sudo nala install build-essential
 
 # Dependencies for ly
-sudo apt install libpam0g-dev libxcb-xkb-dev
+sudo nala install libpam0g-dev libxcb-xkb-dev
 
 # ly install
 git clone --recurse-submodules https://github.com/fairyglade/ly
@@ -71,7 +71,7 @@ make
 sudo make install installsystemd
 sudo systemctl enable ly
 
-reboot
+sudo reboot
 
 
 
